0acf1dba95bacb15bb1e86c2bc3ecf5a82a3143d
tamarariveram <t.riveradg@gmail.com>
2026-02-22 17:19:48 -0300

    Desafío JS inicial

---
+++ assets/scripts/index.js
@@ -0,0 +1,8 @@
+let precio = 400000
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+let cantidadSpan = document.querySelector(".cantidad");
+let totalSpan = document.querySelector(".valor-total")
+y
